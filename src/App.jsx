b7a94f7ccf1052251463ed0e4306866aa9c9595d
kushalgupta1906 <kushalgupta1906@gmail.com>
2025-03-08 17:15:40 +0530

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-blue-500">
       <h1 className=' text-3xl text-white text-center my-3'>Password generator</h1>
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
